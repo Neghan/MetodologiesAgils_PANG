e5a5a7684590d165cfe8d40c52266e9e7f5cd727
Adrià Ortiz <adriaortizmartinez@enti.cat>
2018-11-17 23:45:07 +0100

    Burbujas afectadas por gravedad

--- platformer/js/bubble_prefab.js
+++ platformer/js/bubble_prefab.js
@@ -5,18 +5,20 @@ platformer.bubble_prefab = function(game,x,y,level){
     this.anchor.setTo(.5);
     this.scale.setTo(.75);
     this.animations.add('normal',[0],0,true);
-    this.animations.add('explode',[1,2,3,4],4,false);
+    this.animations.add('explode',[1,2,3,4],10,false);
     this.animations.play('normal');
     this.level = level;
     this.speedX = 30;
-    this.speedY = 30;
+//    this.speedY = 30;
     this.directionX = 1;
-    this.directionY = 1;
-    this.game.physics.arcade.enable(this);
-    this.body.allowGravity = false;
+//    this.directionY = 1;
+    //this.game.physics.arcade.enable(this);
+    this.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.setCircle(23,1);
-    this.destroyDelay = 15;
+    this.destroyDelay = 10;
     this.destroy = false;
+    this.body.bounce.y = 1;
+    this.body.gravity.y = 1; //Parece que a partir de cierto número no baja mas
 };
 
 
@@ -36,17 +38,17 @@ platformer.bubble_prefab.prototype.update = function(){
     this.game.physics.arcade.collide(this,this.level.hero,this.hitHero,null,this);
     
     this.body.velocity.x = this.speedX*this.directionX;
-    this.body.velocity.y = this.speedY*this.directionY;
+    //this.body.velocity.y = this.speedY*this.directionY;
 
    if(this.body.touching.right || this.body.touching.left){
-       this.directionX *=-1;
+        this.directionX *=-1;
         this.body.velocity.x = this.speedX*this.directionX;
    }
        
-   if(this.body.touching.up || this.body.touching.down){
-       this.directionY *=-1;
-       this.body.velocity.y = this.speedY*this.directionY;
-   }
+//   if(this.body.touching.up || this.body.touching.down){
+//       this.directionY *=-1;
+//       this.body.velocity.y = this.speedY*this.directionY;
+//   }
     if (this.destroy == true){
         this.destroyDelay -= 0.3;
         if(this.destroyDelay <= 0){
@@ -60,8 +62,7 @@ platformer.bubble_prefab.prototype.hitHero = function(_bubble,_hero){
     if(_bubble.body.touching && _hero.body.touching){
         
         this.animations.play('explode');
-        this.speedX = 0;
-        this.speedY = 0;
+        this.body.enable = false;
         this.level.hitHero();
         this.destroy = true;
         //delay o quan acabi la animació --> destroy.
