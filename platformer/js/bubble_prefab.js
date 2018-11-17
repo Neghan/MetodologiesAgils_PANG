41f73f37fd2a0f0cbf52767fc125654b7186ab5f
Daniel Olivella <danielom98@gmail.com>
2018-11-17 20:52:08 +0100

    Bubbles con colisiones circulares que se destruyen al colisionar player

--- platformer/js/bubble_prefab.js
+++ platformer/js/bubble_prefab.js
@@ -14,6 +14,9 @@ platformer.bubble_prefab = function(game,x,y,level){
     this.directionY = 1;
     this.game.physics.arcade.enable(this);
     this.body.allowGravity = false;
+    this.body.setCircle(23,1);
+    this.destroyDelay = 15;
+    this.destroy = false;
 };
 
 
@@ -23,6 +26,9 @@ platformer.bubble_prefab.prototype.constructor = platformer.bubble_prefab;
 
 
 platformer.bubble_prefab.prototype.update = function(){
+    
+    //this.game.debug.body(this);
+    
     this.game.physics.arcade.collide(this,this.level.muroLados1);
     this.game.physics.arcade.collide(this,this.level.muroLados2);
     this.game.physics.arcade.collide(this,this.level.muro);
@@ -40,9 +46,15 @@ platformer.bubble_prefab.prototype.update = function(){
    if(this.body.touching.up || this.body.touching.down){
        this.directionY *=-1;
        this.body.velocity.y = this.speedY*this.directionY;
-   } 
-    
+   }
+    if (this.destroy == true){
+        this.destroyDelay -= 0.3;
+        if(this.destroyDelay <= 0){
+            this.kill();
+        }
+    }
 };
+
 
 platformer.bubble_prefab.prototype.hitHero = function(_bubble,_hero){
     if(_bubble.body.touching && _hero.body.touching){
@@ -50,7 +62,8 @@ platformer.bubble_prefab.prototype.hitHero = function(_bubble,_hero){
         this.animations.play('explode');
         this.speedX = 0;
         this.speedY = 0;
-        //this.level.hitHero();
+        this.level.hitHero();
+        this.destroy = true;
         //delay o quan acabi la animació --> destroy.
     }
 };
